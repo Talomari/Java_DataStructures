47fc9905046f52b4f73d1b3c24bd2505e6870936
TalOmari <talomari592@gmail.com>
2018-07-15 12:57:56 +0300

    Javascript

--- Queue.java
+++ Queue.java
@@ -1,13 +1,26 @@
 public class Queue {
    // your code is here
+    int []queue=new int[5] ;
+    int x=0;
     public void push(int pushedElement){
        //your code is here
+        
+        queue[x]=pushedElement;
+        x++;
     }
     public void pop(){
+        for(int i=0;i<queue.length-1;i++){
+            queue[i]=queue[i+1];
+        }
+        x--;
         //your code is here
     }
     public void display(){
         //your code is here
+        for(int i=0;i<queue.length;i++){
+            System.out.println(queue[i]);
+        }
     }
+    
 
 }
